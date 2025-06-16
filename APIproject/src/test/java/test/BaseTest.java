0170e6a93bcd513eb0cec9a72f7e750d5bd4ea3f
Александр Ульяницкий <alex.backend.dev@gmail.com>
2025-06-16 19:09:47 +0300

    Added additional dependency and clean folder

--- APIproject/src/test/java/test/BaseTest.java
+++ APIproject/src/test/java/test/BaseTest.java
@@ -1,9 +1,15 @@
 package test;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import static constants.UrlParamValues.VALID_KEY;
 import static constants.UrlParamValues.VALID_TOKEN;
@@ -12,8 +18,9 @@ public class BaseTest {
 
     @BeforeAll
     public static void setup(){
-        RestAssured.baseURI="https://api.trello.com/1";
         RestAssured.filters(new AllureRestAssured());
+        cleanAllureResults();
+        RestAssured.baseURI="https://api.trello.com/1";
     }
 
     protected static RequestSpecification requestWithAuth(){
@@ -25,5 +32,26 @@ public class BaseTest {
 
     protected static RequestSpecification requestWithoutAuth(){
         return RestAssured.given();
+    }
+
+    private static void cleanAllureResults(){
+        Path allureResultsDir = Paths.get("allure-results");
+
+        if (!Files.exists(allureResultsDir)){
+            Allure.addAttachment("Allure Cleanup", "text/plain",
+                    "Directory 'allure-results' does not exist");
+            return;
+        }
+
+        try {
+            // Используем FileUtils из Apache Commons IO для безопасного удаления
+            FileUtils.cleanDirectory(allureResultsDir.toFile());
+            Allure.addAttachment("Allure Cleanup", "text/plain",
+                    "Successfully cleaned 'allure-results' directory.");
+        } catch (Exception e) {
+            Allure.addAttachment("Allure Cleanup Error", "text/plain",
+                    "Failed to clean 'allure-results': " + e.getMessage());
+            throw new RuntimeException("Failed to clean Allure results directory", e);
+        }
     }
 }
